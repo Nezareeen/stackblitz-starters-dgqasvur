a71251e6f925d7049b432f6f7ed5398a1c656220
Nezareeen <nezareeen@gmail.com>
2025-03-18 13:51:49 +0530

    Complete

---
+++ schema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    type: [String],
+    default: ['user'], // Default role is 'user'
+  },
+  profile: {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+  },
+  lastLogin: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
